9ae358928633e9e903af6fd06f3e90be9a0f79e9
Jie.Yao <yj950714@163.com>
2017-05-15 09:29:55 +0800

    更改页面菜单逻辑

--- js/server.js
+++ js/server.js
@@ -1,2 +1,2 @@
-var server="http://www.sjtuimap.com/network_optimize";
-//var server="http://127.0.0.1/network_optimize"
+//var server="http://www.sjtuimap.com/network_optimize";
+var server="http://127.0.0.1/network_optimize"
